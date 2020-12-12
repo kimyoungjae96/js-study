c1be37b2be2c768f6f20d9db7244a3e2da8cfb70
kimyoungjae <sskim5421@gmail.com>
2020-12-12 20:03:12 +0900

    wrap ErrorBoundary

--- 09/manage-react/src/App.js
+++ 09/manage-react/src/App.js
@@ -1,11 +1,38 @@
-import EventPractice from './EventPractice/EventPractice'
+import EventPractice from "./EventPractice/EventPractice";
+import ValidationSample from "./ValidationSample/ValidationSample";
+import ScrollBox from "./ScrollBox/ScrollBox";
+import LifeCycleSample from "./LifeCycleSample/LifeCycleSample";
+import React, { Component } from "react";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
-function App() {
-  return (
-    <div className="App">
-      <EventPractice/>
-    </div>
-  );
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <ErrorBoundary>
+          <EventPractice />
+          <LifeCycleSample color={this.state.color} />
+          <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <button onClick={() => this.scrollBox.scrollToBottom()}>
+            맨 밑으로
+          </button>
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
