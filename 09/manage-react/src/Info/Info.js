12d9070608c9f910796c7549bd2e3374ef96f5d5
kimyoungjae <sskim5421@gmail.com>
2020-12-13 16:02:34 +0900

    custom hook

--- 09/manage-react/src/Info/Info.js
+++ 09/manage-react/src/Info/Info.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from "react";
+import useInputs from "../UseInputs/useInputs";
 
 function reducer(state, action) {
   return {
@@ -8,14 +9,11 @@ function reducer(state, action) {
 }
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: "",
     nickname: "",
   });
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
   return (
     <div>
       <div>
