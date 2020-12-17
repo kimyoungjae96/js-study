1441dec80b4170379c9e5d40174f409c729c66d4
kimyoungjae <sskim5421@gmail.com>
2020-12-18 00:13:55 +0900

    add useContext

--- 09/manage-react/src/App.js
+++ 09/manage-react/src/App.js
@@ -1,9 +1,20 @@
 import React, { useCallback, useState } from "react";
 import { Route } from "react-router-dom";
 import NewsPage from "./NewsApi/NewsPage";
+import ColorBox from "./ContextTutorial/components/ColorBox";
+import ColorContext, { ColorProvider } from "./ContextTutorial/contexts/color";
+import SelectColors from "./ContextTutorial/components/SelectColors";
 
 const App = () => {
-  return <Route path={"/:category?"} component={NewsPage} />;
+  return (
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+  // return <Route path={"/:category?"} component={NewsPage} />;
 };
 
 export default App;
