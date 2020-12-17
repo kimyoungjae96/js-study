7897bc2e5306f4c576a88bf0dd71d446dbff45f7
kimyoungjae <sskim5421@gmail.com>
2020-12-17 23:18:15 +0900

    create usePromise

--- 09/manage-react/src/App.js
+++ 09/manage-react/src/App.js
@@ -1,17 +1,9 @@
 import React, { useCallback, useState } from "react";
-import axios from "axios";
-import NewsList from "./NewsApi/NewsLIst";
-import Categories from "./NewsApi/Categories";
+import { Route } from "react-router-dom";
+import NewsPage from "./NewsApi/NewsPage";
 
 const App = () => {
-  const [category, setCategory] = useState("all");
-  const onSelect = useCallback((category) => setCategory(category), []);
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  return <Route path={"/:category?"} component={NewsPage} />;
 };
 
 export default App;
--- 09/manage-react/src/NewsApi/NewsLIst.js
+++ 09/manage-react/src/NewsApi/NewsLIst.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "./lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -16,39 +17,25 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "https://google.com",
-  urlToImage: "https://via.placeholder.com/160",
-};
-
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === "all" ? "" : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apikey=bc3d424dbf6a4f79b7bd57951a977f1a`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apikey=bc3d424dbf6a4f79b7bd57951a977f1a`
+    );
   }, [category]);
+  console.log(response);
+
   if (loading) {
     return <NewsListBlock>대기중 ...</NewsListBlock>;
   }
-  if (!articles) {
+  if (!response) {
     return null;
   }
+  if (error) {
+    return <NewsListBlock>에러 발생</NewsListBlock>;
+  }
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
