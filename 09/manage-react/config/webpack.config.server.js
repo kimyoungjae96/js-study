9d23bf5d53fc50a9abb8304a8173a83ad7d91c23
kimyoungjae <sskim5421@gmail.com>
2020-12-25 00:58:14 +0900

    eject & init server side rendering setting

---
+++ 09/manage-react/config/webpack.config.server.js
@@ -0,0 +1,125 @@
+const nodeExternals = require("webpack-node-externals");
+const paths = require("./paths");
+const getCSSModuleLocalIndent = require("react-dev-utils/getCSSModuleLocalIdent");
+const webpack = require("webpack");
+const getClientEnvironment = require("./env");
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+module.exports = {
+  mode: "production",
+  entry: paths.ssrIndexJs,
+  target: "node",
+  output: {
+    path: paths.ssrBuild,
+    filename: "server.js",
+    chunkFilename: "js/[name].chunk.js",
+    publicPath: paths.publicUrlOrPath,
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve("babel-loader"),
+            options: {
+              customize: require.resolve(
+                "babel-preset-react-app/webpack-overrides"
+              ),
+              plugins: [
+                [
+                  require.resolve("babel-plugin-named-asset-import"),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent: "@svgr/webpack?-svgo![path]",
+                      },
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+          //CSS를 위한 처리
+          {
+            test: cssRegex,
+            exclude: cssModuleRegex,
+            loader: require.resolve("css-loader"),
+            options: {
+              onlyLocals: true,
+            },
+          },
+          {
+            test: cssModuleRegex,
+            loader: require.resolve("css-loader"),
+            options: {
+              modules: true,
+              onlyLocals: true,
+              getLocalIdent: getCSSModuleLocalIndent,
+            },
+          },
+          //Sass를 위한 처리
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  onlyLocals: true,
+                },
+              },
+              require.resolve("sass-loader"),
+            ],
+          },
+          // Sass + CSS Module을 위한 처리
+          {
+            test: sassRegex,
+            exclude: sassModuleRegex,
+            use: [
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  modules: true,
+                  onlyLocals: true,
+                  getLocalIdent: getCSSModuleLocalIndent,
+                },
+              },
+              require.resolve("sass-loader"),
+            ],
+          },
+          // url-loader를 위한 설정
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              emitFile: false,
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+          {
+            loader: require.resolve("file-loader"),
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              emitFile: false,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: { modules: ["node_modules"] },
+  externals: [nodeExternals()],
+  plugins: [new webpack.DefinePlugin(env.stringified)],
+};
