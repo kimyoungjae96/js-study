b970c67796d474dedecb0d6e86a658fbc1df5b30
kimyoungjae <sskim5421@gmail.com>
2021-01-03 20:50:55 +0900

    add koa-send & static

--- 09/blog-backend/src/index.js
+++ 09/blog-backend/src/index.js
@@ -5,8 +5,11 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api/index.js';
 import jwtMiddleware from './lib/jwtMiddleware.js';
+import path from 'path';
+import serve from 'koa-static';
+import send from 'koa-send';
 // import createFakeData from './createFakeData.js';
-
+const __dirname = path.resolve();
 dotenv.config();
 const { PORT, MONGO_URI } = process.env;
 
@@ -31,7 +34,17 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 
-app.use(router.routes()).use(router.allowedMethods);
+app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  console.log('엥')
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    console.log('hihi')
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
